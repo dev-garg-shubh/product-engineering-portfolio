3664edc33ab42615f095d65f9254044613f9a141
godShubh <jan22.@gmail.com>
2026-06-21 14:18:43 +0530

    Fix repeated back-to-top scrolling

--- src/components/layout/SiteFooter.tsx
+++ src/components/layout/SiteFooter.tsx
@@ -3,11 +3,17 @@ import { Link } from "react-router-dom";
 import { platformConfig } from "../../config/platform";
 
 export function SiteFooter() {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <footer className="footer">
       <span>{platformConfig.brand.name}</span>
       <div className="footer-links">
-        <Link to="/#top">Back to top <ArrowUp size={15} aria-hidden="true" /></Link>
+        <Link to="/#top" onClick={scrollToTop}>
+          Back to top <ArrowUp size={15} aria-hidden="true" />
+        </Link>
       </div>
     </footer>
   );
