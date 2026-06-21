dbe1d9e775c613c01c8523cdfa80178dca321fdd
godShubh <jan22.@gmail.com>
2026-06-21 14:15:45 +0530

    updated footer

--- src/components/layout/SiteFooter.tsx
+++ src/components/layout/SiteFooter.tsx
@@ -1,4 +1,4 @@
-import { ArrowRight, GitFork } from "lucide-react";
+import { ArrowUp } from "lucide-react";
 import { Link } from "react-router-dom";
 import { platformConfig } from "../../config/platform";
 
@@ -7,11 +7,7 @@ export function SiteFooter() {
     <footer className="footer">
       <span>{platformConfig.brand.name}</span>
       <div className="footer-links">
-        <Link to={platformConfig.developerPage}>For developers <ArrowRight size={15} aria-hidden="true" /></Link>
-        <a href={platformConfig.repositoryUrl} target="_blank" rel="noreferrer">
-          <GitFork size={15} aria-hidden="true" /> Source
-        </a>
-        <Link to="/#top">Back to top <ArrowRight size={15} aria-hidden="true" /></Link>
+        <Link to="/#top">Back to top <ArrowUp size={15} aria-hidden="true" /></Link>
       </div>
     </footer>
   );
